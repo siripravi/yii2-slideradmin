5729a361e8f397e6038ace51f93b5412374617ad
provdigi <purnachandra.valluri@gmail.com>
2023-11-20 07:14:09 +0530

    Update m170310_185835_create_slider_table.php

--- migrations/m170310_185835_create_slider_table.php
+++ migrations/m170310_185835_create_slider_table.php
@@ -14,7 +14,7 @@ class m170310_185835_create_slider_table extends Migration
     {
         $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
        
-        $this->createTable('nxt_slider', [
+        $this->createTable('{{%slider}}', [
             'id' => $this->primaryKey(),            
             'title' => $this->string(),
 			'image_count' => $this->integer(),            
@@ -22,7 +22,7 @@ class m170310_185835_create_slider_table extends Migration
         ], $tableOptions);
         
 		
-        $this->createTable('nxt_slider_image', [
+        $this->createTable('{{%slider_image}}', [
             'id' => $this->primaryKey(),			
             'hash' => $this->string()->notNull(),
 			'filename' => $this->string()->notNull(),
@@ -37,24 +37,19 @@ class m170310_185835_create_slider_table extends Migration
 						
         ], $tableOptions);
 		
-        $this->createTable('nxt_slider_image_lang', [
+        $this->createTable('{{%slider_image_lang}}', [
             'slider_image_id' => $this->integer()->notNull(),
             'lang_id' => $this->string(3)->notNull(),           
             'title' => $this->string()->notNull(),          
             'html' => $this->text(),
            
         ], $tableOptions);
+      
+		$this->addPrimaryKey('pk_nxt_slider_image_lang', '{{%slider_image_lang}}', ['slider_image_id', 'lang_id']);
 
-        /*$this->addPrimaryKey('pk-slider_image_lang', 'nxt_slider_image_lang', ['slider_image_id', 'lang_id']);
-		$this->addForeignKey('fk-slider_image_lang-lang_id', 'nxt_slider_image_lang', 'lang_id', 'nxt_language', 'id', 'CASCADE', 'CASCADE');
-		       
-        $this->addForeignKey('fk-slider_image-slider_id', 'nxt_slider_image', 'slider_id', 'nxt_slider', 'id', 'CASCADE');*/
-		
-		$this->addPrimaryKey('pk_nxt_slider_image_lang', 'nxt_slider_image_lang', ['slider_image_id', 'lang_id']);
+        $this->addForeignKey('fk-nxt_slider_image_lang-slider_image_id', '{{%slider_image_lang}}', 'slider_image_id', '{{%slider_image}}', 'id', 'CASCADE');
 
-        $this->addForeignKey('fk-nxt_slider_image_lang-slider_image_id', 'nxt_slider_image_lang', 'slider_image_id', 'nxt_slider_image', 'id', 'CASCADE');
-
-        $this->addForeignKey('fk-nxt_slider_image_lang-lang_id', 'nxt_slider_image_lang', 'lang_id', 'nxt_language', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk-nxt_slider_image_lang-lang_id', '{{%slider_image_lang}}', 'lang_id', '{{%language}}', 'id', 'CASCADE', 'CASCADE');
 
     }
 
@@ -64,12 +59,12 @@ class m170310_185835_create_slider_table extends Migration
     public function down()
     {
         
-        $this->dropPrimaryKey('pk_nxt_slider_image_lang', 'nxt_slider_image_lang');
-		$this->dropForeignKey('fk-nxt_slider_image_lang-slider_image_id', 'nxt_slider_image_lang');
-        $this->dropForeignKey('fk-nxt_slider_image_lang-lang_id', 'nxt_slider_image_lang');
+        $this->dropPrimaryKey('pk_nxt_slider_image_lang', '{{%slider_image_lang}}');
+		$this->dropForeignKey('fk-nxt_slider_image_lang-slider_image_id', '{{%slider_image_lang}}');
+        $this->dropForeignKey('fk-nxt_slider_image_lang-lang_id', '{{%slider_image_lang}}');
 		       
-        $this->dropTable('nxt_slider');
-        $this->dropTable('nxt_slider_image');
-        $this->dropTable('nxt_slider_image_lang');
+        $this->dropTable('{{%slider}}');
+        $this->dropTable('{{%slider_image}}');
+        $this->dropTable('{{%slider_image_lang}}');
     }
 }
