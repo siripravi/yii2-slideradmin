66d48558ffc08d4eccf97d249eda5ad70385858a
provdigi <purnachandra.valluri@gmail.com>
2023-11-20 07:13:36 +0530

    Update m161028_105921_create_language_table.php
    updated migration

--- migrations/m161028_105921_create_language_table.php
+++ migrations/m161028_105921_create_language_table.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `language`.
+ * Handles the creation of table `slider`.
  */
-class m161028_105921_create_language_table extends Migration
+class m170310_185835_create_slider_table extends Migration
 {
     /**
      * @inheritdoc
@@ -13,19 +13,44 @@ class m161028_105921_create_language_table extends Migration
     public function up()
     {
         $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
-
-        $this->createTable('language', [
-            'id' => $this->string(3)->notNull(),
-            'name' => $this->string(31)->notNull(),
-            'position' => $this->smallInteger()->defaultValue(0),
-            'enabled' => $this->boolean()->notNull()->defaultValue(1)
+       
+        $this->createTable('{{%slider}}', [
+            'id' => $this->primaryKey(),            
+            'title' => $this->string(),
+			'image_count' => $this->integer(),            
+            'enabled' => $this->boolean()->notNull()->defaultValue(1),
         ], $tableOptions);
+        
+		
+        $this->createTable('{{%slider_image}}', [
+            'id' => $this->primaryKey(),			
+            'hash' => $this->string()->notNull(),
+			'filename' => $this->string()->notNull(),
+            'extension' => $this->string()->notNull(),
+			'mimeType' => $this->string()->notNull(),
+			'byteSize' => $this->integer(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+            'position' => $this->integer()->notNull()->defaultValue(0),
+            'enabled' => $this->boolean()->notNull()->defaultValue(1),
+			'slider_id'  => $this->integer(),
+						
+        ], $tableOptions);
+		
+        $this->createTable('{{%slider_image_lang}}', [
+            'slider_image_id' => $this->integer()->notNull(),
+            'lang_id' => $this->string(3)->notNull(),           
+            'title' => $this->string()->notNull(),          
+            'html' => $this->text(),
+           
+        ], $tableOptions);
+      
+		$this->addPrimaryKey('pk_nxt_slider_image_lang', '{{%slider_image_lang}}', ['slider_image_id', 'lang_id']);
 
-        $this->addPrimaryKey('id', 'language', 'id');
+        $this->addForeignKey('fk-nxt_slider_image_lang-slider_image_id', '{{%slider_image_lang}}', 'slider_image_id', '{{%slider_image}}', 'id', 'CASCADE');
 
-        $this->batchInsert('language', ['id', 'name', 'position'], [
-            ['en', 'English', 1],['hi', 'Hindi', 1],
-        ]);
+        $this->addForeignKey('fk-nxt_slider_image_lang-lang_id', '{{%slider_image_lang}}', 'lang_id', '{{%language}}', 'id', 'CASCADE', 'CASCADE');
+
     }
 
     /**
@@ -33,6 +58,13 @@ class m161028_105921_create_language_table extends Migration
      */
     public function down()
     {
-        $this->dropTable('language');
+        
+        $this->dropPrimaryKey('pk_nxt_slider_image_lang', '{{%slider_image_lang}}');
+		$this->dropForeignKey('fk-nxt_slider_image_lang-slider_image_id', '{{%slider_image_lang}}');
+        $this->dropForeignKey('fk-nxt_slider_image_lang-lang_id', '{{%slider_image_lang}}');
+		       
+        $this->dropTable('{{%slider}}');
+        $this->dropTable('{{%slider_image}}');
+        $this->dropTable('{{%slider_image_lang}}');
     }
 }
