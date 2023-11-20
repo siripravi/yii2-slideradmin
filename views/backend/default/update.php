4a576edb4a61bf3f5640a1e2618d3ea7fa45cbec
provdigi <purnachandra.valluri@gmail.com>
2023-11-20 12:05:27 +0530

    Update update.php

--- views/backend/default/update.php
+++ views/backend/default/update.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $model common\modules\products\models\Brand */
 
 $this->title = Yii::t('app', 'Update {modelClass}: ', [
-    'modelClass' => 'Brand',
+    'modelClass' => 'Slider',
 ]) . $model->id;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Brands'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
